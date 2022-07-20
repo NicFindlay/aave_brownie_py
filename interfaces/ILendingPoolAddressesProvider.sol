8258a623c7116317274e44ee3b60636e65a9e97a
Nic <nicisme@gmail.com>
2022-07-20 15:38:33 +0200

    Adding interfaces for Aave

---
+++ interfaces/ILendingPoolAddressesProvider.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.6.12;
+
+/**
+ * @title LendingPoolAddressesProvider contract
+ * @dev Main registry of addresses part of or connected to the protocol, including permissioned roles
+ * - Acting also as factory of proxies and admin of those, so with right to change its implementations
+ * - Owned by the Aave Governance
+ * @author Aave
+ **/
+interface ILendingPoolAddressesProvider {
+    function getLendingPool() external view returns (address);
+
+    function setLendingPoolImpl(address pool) external;
+
+    function getLendingPoolConfigurator() external view returns (address);
+
+    function setLendingPoolConfiguratorImpl(address configurator) external;
+
+    function getLendingPoolCollateralManager() external view returns (address);
+
+    function setLendingPoolCollateralManager(address manager) external;
+
+    function getPoolAdmin() external view returns (address);
+
+    function setPoolAdmin(address admin) external;
+
+    function getEmergencyAdmin() external view returns (address);
+
+    function setEmergencyAdmin(address admin) external;
+
+    function getPriceOracle() external view returns (address);
+
+    function setPriceOracle(address priceOracle) external;
+
+    function getLendingRateOracle() external view returns (address);
+
+    function setLendingRateOracle(address lendingRateOracle) external;
+}
